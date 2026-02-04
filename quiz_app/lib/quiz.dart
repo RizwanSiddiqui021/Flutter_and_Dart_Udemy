d10914260ba072260ecbbf287ddc1c3deb285b0d
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2026-02-04 15:28:52 +0600

    completed upto mealDetails screen

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -50,10 +50,7 @@ class _QuizState extends State<Quiz> {
         body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
+              colors: [Colors.purple, Colors.deepPurpleAccent],
               begin: AlignmentGeometry.topLeft,
               end: AlignmentGeometry.bottomRight,
             ),
