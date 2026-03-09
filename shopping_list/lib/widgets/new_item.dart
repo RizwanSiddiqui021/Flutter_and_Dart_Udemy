16830547a1ab5088794185aa3160af57761e5f35
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2026-03-09 14:48:24 +0600

    finished module 11

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -19,10 +20,12 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      Navigator.of(context).pop(GroceryItem(id: DateTime.now().toString(),
+      name: _itemName,
+      quantity: _enteredQuantity,
+      category: _selectedCategory));
     }
-    print(_itemName);
-    print(_enteredQuantity);
-    print(_selectedCategory);
+
   }
 
   @override
