e2feeedcd1c74e2f74d6c52a833b79a208103eb0
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2026-04-05 17:15:30 +0600

    completed upto module 13 and video 14

---
+++ favorite_places/lib/screens/add_place.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+
+    if (enteredTitle.isEmpty || _selectedImage == null) { 
+      return;
+    }
+
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!);
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Add New Place")),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(label: Text("Title")),
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+              controller: _titleController,
+            ),
+            const SizedBox(height: 12),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton.icon(
+              onPressed: _savePlace,
+              icon: Icon(Icons.add),
+              label: Text("Add Place"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
